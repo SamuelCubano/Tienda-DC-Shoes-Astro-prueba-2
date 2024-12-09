ba3bbdffc5d94fd3d081ba1f2f12e16fcf6eea82
SamCub <samule10cubano@gmail.com>
2024-12-08 21:06:17 -0400

    Prueba2

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://SamuelCubano.github.io',
+  base: 'Prueba-2-con-Astro',
+    devToolbar: {
+        enabled: false
+      }
+});
